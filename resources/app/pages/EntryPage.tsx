077421ad58efdfd2387f2dc0195f2c723d6d6983
jiasiang2015 <jiasiang_gao@imagtw.com>
2024-07-12 16:53:23 +0800

    [Add] 添加 maxwell cat

--- resources/app/pages/EntryPage.tsx
+++ resources/app/pages/EntryPage.tsx
@@ -1,7 +1,11 @@
+import MaxwellCat from '@/assets/images/maxwell-cat.gif'
+
 export default function EntryPage() {
   return (
-    <div>
-      <h1>EntryPage</h1>
+    <div style={{ padding: "10vmin" }}>
+      <img src={MaxwellCat} alt="Maxwell Cat" />
+      <h1>The Maxwll Cat Say Hello to you！</h1>
+      <h2>Entry Page.</h2>
     </div>
   )
 }
